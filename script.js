f8f5fc069b69ff926d410a7bd9ac05070beb6341
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-28 15:30:34 +0100

    add createGrid function to make grid of squares

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const container = document.querySelector(".container");
+// calculate the height/width of a square based on the container sizing
+/* function getSquareDimensions() {
+  const container = document.getElementById("container")
+  const cssObj = window.getComputedStyle(container);
+}
+ */
 
-let gridSquare = document.createElement("div");
-gridSquare.className = "square";
+function createGrid() {
+  const container = document.getElementById("container");
 
-container.appendChild(gridSquare);
+  let gridSquare = document.createElement("div");
+
+  gridSquare.className = "square";
+
+  container.appendChild(gridSquare);
+  
+  for (i = 1; i < 256; i++) {
+    let newSquare = gridSquare.cloneNode(true);
+    container.appendChild(newSquare);
+  }  
+}
+
+createGrid();
