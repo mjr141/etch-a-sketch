bd599fb6b79f303b61641af2c8293f72d1a26ab7
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-05-08 13:05:31 +0100

    have button prompt create new grid

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const containerWidth = 512; // width in pixels
 function createGrid(sqPerSide = 16, sqWidth = 32) {
   // add the first square to the grid and set dimensions
   const container = document.getElementById("container");
+  container.replaceChildren(); // remove all child elements from container so we start with a blank grid
   let gridSquare = document.createElement("div");
   gridSquare.className = "square";
   container.appendChild(gridSquare);
@@ -20,39 +21,39 @@ function createGrid(sqPerSide = 16, sqWidth = 32) {
     container.appendChild(newSquare);
   }
 
-/*   let squares = document.querySelectorAll(".square").forEach(function(sq) {
-    sq.style.width = sqWidth;
-    sq.style.height = sqWidth;
-  }); */
+  // add on-click behaviour for newly created squares
+  const squares = document.getElementsByClassName("square");
+  for (i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      e.target.style.backgroundColor = "black";
+    });
+    squares[i].addEventListener("mouseover", (e) => {
+      console.log((e).buttons);
+      if(e.buttons === 1) {
+        e.target.style.backgroundColor = "black";
+      } 
+    });
+}
 }
 
 createGrid(); // initialize the grid with default values
 
-const squares = document.getElementsByClassName("square");
-for (i = 0; i < squares.length; i++) {
-  squares[i].addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    e.target.style.backgroundColor = "black";
-  }
-  );
-  squares[i].addEventListener("mouseover", (e) => {
-    console.log((e).buttons);
-    if(e.buttons === 1) {
-      e.target.style.backgroundColor = "black";
-    } 
-  }
-  );
-}
-
 let button = document.querySelector("button");
 button.addEventListener("click", function() {
   let newGridCount = prompt("select the number of squares per side for a new grid (default: 16)");
-  if (Number.isInteger(+newGridCount) && newGridCount <= 100) {
+  if (newGridCount === "") { // user pressed ok but input nothing
+    createGrid()
+  } else if (newGridCount === null) {
+    // user pressed cancel
+    return;
+  } else if (Number.isInteger(+newGridCount) && newGridCount > 0 && newGridCount <= 100) { 
+    // user input a valid number
+    // note that +null is 0
     newSquareSize = containerWidth/newGridCount;
-    alert(`number of squares per side: ${newGridCount}`);
-    alert(`width/height of a new square: ${newSquareSize}`);
+    createGrid(newGridCount, newSquareSize);
   } else {
+    // user input an invalid number and did not hit cancel
     alert("not a valid number");
   }
-}
-);
+});
